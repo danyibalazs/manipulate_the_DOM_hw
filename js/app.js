94fec3b44d022c76c039a838a5dd36b596a9d90f
danyibalazs <danyi.balage@gmail.com>
2020-02-23 12:41:43 +0000

    add css to form

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const newItemform = document.querySelector('#new-item-form');
   newItemform.addEventListener('submit', handleNewItemFormSubmit);
+
+  const getForm = document.querySelector('#form-link');
+  getForm.addEventListener('click', showForm);
+
+  const getTraining = document.querySelector('#training-link');
+  getTraining.addEventListener('click', showTraining);
 })
 
 const handleNewItemFormSubmit = function (event) {
@@ -11,6 +17,23 @@ const handleNewItemFormSubmit = function (event) {
   trainingList.appendChild(trainingListItem);
 
   event.target.reset();
+  showTraining();
+}
+
+const showForm = function (event) {
+  const form = document.getElementById("exercise-form");
+  form.classList.remove("hide");
+
+  const list = document.getElementById("training-view");
+  list.classList.add("hide");
+}
+
+const showTraining = function (event) {
+  const list = document.getElementById("training-view");
+  list.classList.remove("hide");
+
+  const form = document.getElementById("exercise-form");
+  form.classList.add("hide");
 }
 
 const createTrainingListItem = function (form) {
